138b12e27ec89c3e8a966c6ed1750d950e52b4d2
ParkerKemp <charles.kemp702@topper.wku.edu>
2014-07-18 22:50:08 -0500

    Initial commit.

---
+++ src/com/spinalcraft/bigbrother/BigBrother.java
@@ -0,0 +1,17 @@
+package com.spinalcraft.bigbrother;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BigBrother extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
